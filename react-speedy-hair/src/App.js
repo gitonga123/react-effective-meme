5d37703e16a2cb57d06d921e805f541d996a5f78
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-07-16 20:28:10 +0300

    React life cycle: Unmounting

--- react-speedy-hair/src/App.js
+++ react-speedy-hair/src/App.js
@@ -65,12 +65,50 @@ class Header extends React.Component {
          </h1>
          <div id="div1"></div>
          <div id="div2"></div>
-         {/* <button type="button" onClick={this.changeColor}>Change Color</button> */}
+        <button type="button" onClick={this.changeColor}>Change Color</button>
        </div>
      );
   }
 }
  
-export default Header;
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: true }
+  }
+  delHeader = () => {
+    this.setState({show: false})
+  }
+
+  render() { 
+    let myHeader;
+      if (this.state.show) {
+        myHeader = <Child />
+      };
+    return ( 
+      <div>
+        {myHeader}
+        <button type="button" onClick={this.delHeader}>
+          Delete Header
+        </button>
+      </div>
+     );
+  }
+}
+
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("The component named Header is about to be unmounted.");
+  }
+  render() { 
+    return ( <h1>Hello World!</h1> );
+  }
+}
+ 
+// export default Child;
+ 
+export default Container;
+ 
+// export default Header;
 
 // export default Car;
