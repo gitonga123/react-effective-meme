9477f0d7043017d06cc47a345f223265d447cd6d
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-07-16 20:33:13 +0300

    HTML React Events

--- react-speedy-hair/src/index.js
+++ react-speedy-hair/src/index.js
@@ -3,4 +3,18 @@ import ReactDOM from 'react-dom'
 import Header from './App';
 
 
-ReactDOM.render(<Header favCol="Yellow"/>, document.getElementById('root'));
+// ReactDOM.render(<Header favCol="Yellow"/>, document.getElementById('root'));
+class Football extends React.Component {
+    shoot() {
+        alert("Great Shot!");
+    }
+    render() {
+        return (
+            <button onClick={this.shoot}>
+                Take the shot!
+            </button>
+        );
+    }
+}
+
+ReactDOM.render(<Football />, document.getElementById('root'))
