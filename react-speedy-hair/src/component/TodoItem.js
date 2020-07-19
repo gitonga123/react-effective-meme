353e126cf4e31901092e014e78731a675505f5d2
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-07-19 09:05:26 +0300

    Covert function component to class component

--- react-speedy-hair/src/component/TodoItem.js
+++ react-speedy-hair/src/component/TodoItem.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
-function TodoItem(props) {
-    return (
-        <div className="todo-item">
-            <input type="checkbox" checked={props.completed}/>
-            <p>{props.text}</p>
-        </div>
-    )
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { completed: props.completed }
+    }
+    updateTaskComplete = () => {
+        this.setState({completed: !this.state.completed});
+    }
+    render() { 
+        return ( 
+            <div className="todo-item">
+                <input type="checkbox" onChange={this.updateTaskComplete} checked={this.state.completed}/>
+                <p>{this.props.text}</p>
+            </div>
+        );
+    }
 }
-
+ 
 export default TodoItem;
