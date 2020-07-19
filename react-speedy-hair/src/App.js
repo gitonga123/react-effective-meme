df983e053954fa1c8b8e44a38d75e427e7500d77
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-07-19 08:56:47 +0300

    Todo items components map

--- react-speedy-hair/src/App.js
+++ react-speedy-hair/src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './component/Header';
 import TodoItem from './component/TodoItem'
+import todoData from './todosData'
 
 class App extends React.Component {
   render() {
+    const todosData = todoData.map(todoItem => <TodoItem key={todoItem.id} text={todoItem.text} completed={todoItem.completed} />)
     return (
       <div className="todo-list">
         <Header />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todosData}
       </div>
     )
   }
