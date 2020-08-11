9597f1852f75708c7f2a7511d6f763ba97a50020
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-08-11 15:01:11 +0300

    A simple node module

--- refresh-node/test_node.js
+++ refresh-node/test_node.js
@@ -1,6 +1,9 @@
 var http = require('http');
 
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end("Hello World\n");
-}).listen(8081)
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+  console.log("Started running");
+}).listen(8080);
+
+console.log("Started running");
