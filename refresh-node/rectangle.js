bfc549ed03c11fcb5352c034b55f778f5e49cc4b
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-08-11 16:01:33 +0300

    Js Callback functions

--- refresh-node/rectangle.js
+++ refresh-node/rectangle.js
@@ -1,8 +1,24 @@
-var rect = {
-    perimeter: (x, y) => (2 * (x + y)),
-    area: (x, y) => (x * y)
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    setTimeout(
+      () =>
+        callback(
+          new Error(
+            `Rectangle dimensions should be greater than zero = ${x} and y = ${y}`
+          ),
+          null
+        ),
+      2000
+    );
+  } else {
+    setTimeout(() => callback(null, {
+        perimeter: () => 2 * (x + y),
+        area: () => x * y,
+      }), 1000);
+  }
 };
 
-exports.perimeter = (x, y) => (2 * (x + y));
-exports.area = (x, y) => (x * y);
-exports.rect;
+
+// exports.perimeter = (x, y) => (2 * (x + y));
+// exports.area = (x, y) => (x * y);
+// exports.rect;
