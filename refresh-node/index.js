4cf167ea84dbe1f07416858bdb6adcd6f3479cc8
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-08-11 19:33:31 +0300

    Simple MongoDb Crud operation - NodeJS

--- refresh-node/index.js
+++ refresh-node/index.js
@@ -1,57 +1,46 @@
-const http = require('http')
-const fs = require('fs');
-const path = require('path');
-const hostname = 'localhost';
+// const MongoClient = require("mongodb").MongoClient;
+const dbOperations = require("./operations");
 
-const port = 3000;
+const assert = require("assert");
+const { MongoClient } = require("mongodb");
 
-const server = http.createServer((request, response)  => {
-    
-    response.setHeader('Content-Type', 'text/html')
-    var fileUrl = '/index.html';
-    let baseFilePath = path.resolve('./public')
-    if (request.url == "/") {
-        let filePath = baseFilePath+fileUrl;
-        const fileExt = path.extname(filePath);
-        if (fileExt == '.html') {
-            fs.exists(filePath, (exists) => {
-                if (!exists) {
-                    filePath = baseFilePath + "/404.html";
-                    response.statusCode = 404;
-                    fs.createReadStream(filePath).pipe(response)
-                    return;
-                }
-                fs.createReadStream(filePath).pipe(response)
+const url = "mongodb://localhost:27017";
+const dbname = "conFusion";
+
+MongoClient.connect(url, (err, client) => {
+  assert.equal(err, null);
+  console.log("Database connection established successfully");
+  const db = client.db(dbname);
+  dbOperations.insertDocument(
+    db,
+    {
+      name: "Pizza Inn Kenya",
+      description: "Test",
+    },
+    "dishes",
+    (result) => {
+      console.log(`Insert Document:\n ${result.ops}`);
+      dbOperations.findDocuments(db, "dishes", (docs) => {
+        console.log(`Found Documents:\n${docs}`);
+
+        dbOperations.updateDocument(
+          db,
+          { name: "Pizza Inn Kenya" },
+          { description: "This restaurant is located in kenya" },
+          "dishes",
+          (result) => {
+            console.log(`Updated Document Successfully:\n ${result.result}`);
+            dbOperations.findDocuments(db, "dishes", (docs) => {
+              console.log(`Found updated Documents:\n${docs}`);
+              db.dropCollection("dishes", (result) => {
+                console.log("Dropped Collection: ", result);
+
+                client.close();
+              });
             });
-        } else {
-            filePath = baseFilePath + "/404.html";
-            response.statusCode = 404;
-            fs.createReadStream(filePath).pipe(response)
-            return;
-        }
-    } else {
-        fileUrl = 'about.html'
-        let filePath = baseFilePath + fileUrl;
-        const fileExtension = path.extname(filePath);
-        if (fileExtension == '.html') {
-            fs.exists(filePath, (exists) => {
-                if (!exists) {
-                    filePath = baseFilePath + "/404.html";
-                    response.statusCode = 404;
-                    fs.createReadStream(filePath).pipe(response)
-                    return;
-                }
-                fs.createReadStream(filePath).pipe(response)
-            });
-        } else {
-            filePath = baseFilePath + "/404.html";
-            response.statusCode = 404;
-            fs.createReadStream(filePath).pipe(response)
-            return;
-        }
+          }
+        );
+      });
     }
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`)
+  );
 });
