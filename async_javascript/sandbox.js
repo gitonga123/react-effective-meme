912bfb2abc415aebfc1d25131b8b40fe5064bc25
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-10-22 19:27:47 +0300

    Async Js - Callback hell

--- async_javascript/sandbox.js
+++ async_javascript/sandbox.js
@@ -1,19 +1,23 @@
-const getTodos = (callback) => {
-    const request = new XMLHttpRequest();
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            callback(null, request.responseText);
-        } else if (request.readyState === 4 ) {
-            callback("Error: Data not found.", null);
-        }
-    });
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');request.send();
-}
-
-getTodos((err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
+const getTodos = (resource, callback) => {
+  const request = new XMLHttpRequest();
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const data = JSON.parse(request.responseText);
+      callback(null, data);
+    } else if (request.readyState === 4) {
+      callback("Error: Data not found.", null);
     }
+  });
+  request.open("GET", resource);
+  request.send();
+};
+
+getTodos("https://jsonplaceholder.typicode.com/todos", (err, data) => {
+  console.log(data);
+  getTodos("https://jsonplaceholder.typicode.com/posts", (err, data) => {
+    console.log(data);
+    getTodos("https://jsonplaceholder.typicode.com/users", (err, data) => {
+      console.log(data);
+    });
+  });
 });
