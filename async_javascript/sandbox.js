d2a80930227899bea4002b49a2789681800a4afb
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-10-22 18:54:26 +0300

    Async Js - Http requests, status

--- async_javascript/sandbox.js
+++ async_javascript/sandbox.js
@@ -2,8 +2,10 @@ const request = new XMLHttpRequest();
 
 
 request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4) {
-        console.log(request.responseText);
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request.responseText)
+    } else if (request.readyState === 4 ) {
+        console.log("Error: Could not fetch the data");
     }
 });
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos');request.send();
