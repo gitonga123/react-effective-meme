9471755c4e72fd5b2d6d84747f9053e9e12eef08
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-10-23 18:54:16 +0300

    Asyn js - Throwing errors

--- async_javascript/sandbox.js
+++ async_javascript/sandbox.js
@@ -2,6 +2,10 @@
 
 const getTodos = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/todoss');
+
+    if (response.status !== 200 ) {
+        throw new Error('Cannot fetch the data');
+    }
     const data = await response.json();
     return data;
 }
