2f4b0941653261c26f28b755db5240c54a31de15
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-10-23 18:27:03 +0300

    Asyn js - fetch api

--- async_javascript/sandbox.js
+++ async_javascript/sandbox.js
@@ -1,33 +1,10 @@
-const getTodos = resource => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.addEventListener("readystatechange", () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject("Error: Data not found.", null);
-      }
+// Fetch Api
+fetch('https://jsonplaceholder.typicode.com/todos').then(response => {
+    return response.json();
+}).then(data => {
+    data.forEach(element => {
+        console.log(element.title);
     });
-    request.open("GET", resource);
-    request.send();
-  });
-};
-
-getTodos('https://jsonplaceholder.typicode.com/todos').then(data => {
-  data.forEach(element => {
-    console.log(element.title);
-  });
-  return getTodos('https://jsonplaceholder.typicode.com/users');
-}).then(data => {
-  data.forEach(element => {
-    console.log(element.id);
-  });
-  return getTodos('https://jsonplaceholder.typicode.com/posts');
-}).then(data => {
-  data.forEach(element => {
-    console.log(element.id);
-  });
-}).catch(error => {
-  console.log(error);
+}).catch(err => {
+    console.log(err);
 });
