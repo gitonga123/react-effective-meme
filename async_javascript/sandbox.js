98216c190cae4026bc89c5f2f78e0d44a5670179
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2020-10-23 18:12:14 +0300

    Asyn js - promises

--- async_javascript/sandbox.js
+++ async_javascript/sandbox.js
@@ -1,23 +1,23 @@
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(null, data);
-    } else if (request.readyState === 4) {
-      callback("Error: Data not found.", null);
-    }
+const getTodos = resource => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("Error: Data not found.", null);
+      }
+    });
+    request.open("GET", resource);
+    request.send();
   });
-  request.open("GET", resource);
-  request.send();
 };
 
-getTodos("https://jsonplaceholder.typicode.com/todos", (err, data) => {
-  console.log(data);
-  getTodos("https://jsonplaceholder.typicode.com/posts", (err, data) => {
-    console.log(data);
-    getTodos("https://jsonplaceholder.typicode.com/users", (err, data) => {
-      console.log(data);
-    });
+getTodos('https://jsonplaceholder.typicode.com/todos').then(data => {
+  data.forEach(element => {
+    console.log(element.title);
   });
+}).catch(error => {
+  console.log(error);
 });
