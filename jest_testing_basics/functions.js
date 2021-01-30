5e803b5b958a593341a765b2a58cae6a2c6e0f4c
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2021-01-30 17:18:26 +0300

    Test to be nul

--- jest_testing_basics/functions.js
+++ jest_testing_basics/functions.js
@@ -3,6 +3,7 @@ const functions = {
     subtract: (num1, num2) => num1 - num2,
     divide: (num1, num2) => num1 / num2,
     multiply: (num1, num2) => num1 * num2,
+    isNul: () => null
 }
 
 module.exports = functions;
--- jest_testing_basics/functions.test.js
+++ jest_testing_basics/functions.test.js
@@ -1,6 +1,6 @@
 const functions = require('./functions');
 
-test('should add 2 + 2 and result 4', () => {
+test('should add 2 + 2 and result to 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
 
@@ -8,3 +8,6 @@ test('Adds 2 + 2 to NOT EQUAL 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 });
 
+test('should be null', () => {
+    expect(functions.isNul()).toBeNull();
+})
